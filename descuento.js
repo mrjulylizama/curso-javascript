7e9e6fb440eca3037d556aa484e6025981e2ebd1
Julio Hernández <julio.lizama@hotmail.com>
2022-03-16 11:54:42 -0600

    Usando error first

--- descuento.js
+++ descuento.js
@@ -8,6 +8,32 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+function onClicButtonPriceDiscount() {
+
+    const cupones = [
+        "Marco polo",
+        "Se la vá o se la piensa",
+        "Pues si mi cielo",
+    ];
+
+    const inputPrice = parseFloat(document.getElementById("inputPrice").value);
+    const inputCupon = document.getElementById("inputCupon").value;
+
+    if (!cupones.includes(inputCupon)) {
+        alert("El cupón " + inputCupon + " no es válido");
+    } else if (inputCupon === "Marco polo") {
+        descuento = 15;
+    } else if(inputCupon === "Se la vá o se la piensa") {
+        descuento = 25;
+    } else if(inputCupon === "Pues si mi cielo") {
+        descuento = 30;
+    }; 
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrice, descuento);
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = "El precio con descuento es $" + precioConDescuento;
+}
+
 // console.log({
 //     precioOriginal,
 //     descuento,
